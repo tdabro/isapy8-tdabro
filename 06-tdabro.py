382bf33c3ddef83914045c15de9997b5e05b1773
Tomasz Dabrowski <tomasz.dabrowski@acxiom.com>
2018-09-26 20:24:15 +0200

    message 1

--- 06-tdabro.py
+++ 06-tdabro.py
@@ -1,15 +1,10 @@
-True
-False
 
-print(True and True)
-print(True and False)
-print(False and True)
-print(False and False)
+def fibon(n):
+    x,y,i=1,1,1
 
-print(54 % 2 == 0 and 69 // 23 == 3)
+    while i <= n:
+        x, y = x + y, x
+        print("y="+str(y))
+        i=i+1
 
-print(True or True)
-print(True or False)
-print(False or True)
-print(False or False)
-print(not False or False)
+print(fibon(5))
